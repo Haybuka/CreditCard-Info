db30f8f21e3e3d40aa40bcc9d5505c2c40cb0b31
Haybuka <paschal.nduleo@gmail.com>
2020-06-07 18:03:12 +0100

    Updated Number input synced the input field to display on card Each 4 numbers belongs to a target li from the html Each li in the Html has a grid attached to it,which is responsble for the gap between elements

--- main.js
+++ main.js
@@ -7,18 +7,43 @@ const submit = document.querySelector('input#submit')
 const form = document.querySelector('form#form')
 const backcard = document.querySelector('.backcard')
 const cvvSpan = document.querySelector('.white span')
+const first = document.querySelector('.card .inputhere .first')
+const second = document.querySelector('.card .inputhere .second')
+const third = document.querySelector('.card .inputhere .third')
+const fourth = document.querySelector('.card .inputhere .fourth')
 const userData = {};
 const cardInfo = [cardNumber, cardHolder, month, year, cvv];
 const card = document.querySelector('.card')
-
-cvv.addEventListener('focus', (e) => {
+//handling Card Num Input
+cardNumber.addEventListener('input', () => {
+    //split of string to occupy the display on the card
+    const num = cardNumber.value;
+    firstFour = num.slice(0, 4);
+    secondFour = num.slice(4, 8)
+    thirdFour = num.slice(8, 12)
+    fourthfour = num.slice(12)
+    // Assigning each num length to li in css
+    first.textContent = firstFour;
+    second.textContent = secondFour;
+    third.textContent = thirdFour;
+    fourth.textContent = fourthfour;
+})
+// handling cvv field and rotation
+const rotate = () => {
     card.classList.toggle('rotateF')
     backcard.classList.toggle('rotateB')
-})
-cvv.addEventListener('blur', (e) => {
-    card.classList.toggle('rotateF')
-    backcard.classList.toggle('rotateB')
-})
+}
+cvv.addEventListener('focus', rotate)
+cvv.addEventListener('blur', rotate)
+//cvv input to text content
 cvv.addEventListener('input', (e) => {
     cvvSpan.textContent = cvv.value;
+})
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    cvv.textContent = " "
+    cardNumber.textContent = " "
+    cardHolder.textContent = " "
+    month.textContent = " "
+    year.textContent == " "
 })
